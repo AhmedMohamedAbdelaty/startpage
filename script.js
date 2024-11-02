7a3ae277ceaaa92774c7f3a1b4fe9689aae249c5
Ahmed Muhammed Abdelaty <am0103738@gmail.com>
2024-11-02 18:25:12 +0200

    display date, edit icons for links

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-'use strict';
+`use strict`;
 
 // display time as hh:mm
 const options = {
-    timeZone:"Africa/Cairo",
+    timeZone : Intl.DateTimeFormat().resolvedOptions().timeZone, // Automatically get user's timezone
     hour12 : true,
     hour:  "2-digit",
     minute: "2-digit",
 }
 
-var today = new Date();
+const locale = navigator.languages != undefined ? navigator.languages[0] : navigator.language; // Get user's locale to format clock accordingly
 
-var time = today.toLocaleTimeString("en-US", options);
+myTimer(); // Initialise clock otherwise you wait a second before it appears
+myDate(); // Initialise date
 
-document.getElementById("time").textContent = time;
+var myVar = setInterval(function () {
+    myTimer();
+}, 1000);
+
+function myTimer() {
+    var today = new Date();
+
+    var time = today.toLocaleTimeString(locale, options);
+
+    document.getElementById("time").textContent = time;
+}
+
+function myDate() {
+    var today = new Date();
+    var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var date = today.toLocaleDateString(locale, dateOptions);
+    document.getElementById("date").textContent = date;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    myDate(); // Update date on page load
+});
